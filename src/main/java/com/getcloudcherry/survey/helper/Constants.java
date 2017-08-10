b96dbf42886a7598ad6a75d1eb132a4a56d7cb8d
ritesh-dubey <riteshdubey2408@gmail.com>
2017-08-10 17:15:17 +0530

    Upgraded Retrofit and Added support for Nougat (#29)

--- src/main/java/com/getcloudcherry/survey/helper/Constants.java
+++ src/main/java/com/getcloudcherry/survey/helper/Constants.java
@@ -2,6 +2,8 @@ package com.getcloudcherry.survey.helper;
 
 import android.util.Log;
 
+import com.getcloudcherry.survey.BuildConfig;
+
 /**
  * Created by riteshdubey on 8/10/16.
  */
@@ -12,32 +14,32 @@ public class Constants {
     public static final String AUTHORIZATION_BEARER = "Bearer";
 
     public static final void logVerbose(String iTitle, String iLogMessage) {
-        if (true)
+        if (BuildConfig.DEBUG)
             Log.v(Constants.TAG, iTitle + " : " + iLogMessage);
     }
 
     public static final void logInfo(String iTitle, String iLogMessage) {
 
-        if (true) {
+        if (BuildConfig.DEBUG) {
             Log.i(Constants.TAG, iTitle + " : " + iLogMessage);
         }
     }
 
     public static final void logError(String iTitle, String iLogMessage,
                                       Exception iException) {
-        if (true)
+        if (BuildConfig.DEBUG)
             Log.e(Constants.TAG, iTitle + " : " + iLogMessage, iException);
 
     }
 
     public static final void logDebug(String iTitle, String iLogMessage) {
-        if (true)
+        if (BuildConfig.DEBUG)
             Log.d(Constants.TAG, iTitle + " : " + iLogMessage);
 
     }
 
     public static final void logWarn(String iTitle, String iLogMessage) {
-        if (true)
+        if (BuildConfig.DEBUG)
             Log.w(Constants.TAG, iTitle + " : " + iLogMessage);
 
     }
